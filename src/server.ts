bf041964eccc14957a794cca426ca92b639f376c
MehtabAlamKhan <kmehtabalam19@gmail.com>
2024-10-14 18:39:34 +0530

    added transmuxing server

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,3 @@
-import Net from "node:net";
-import tls from "node:tls";
-import path from "node:path";
-import RtmpConnectionPool from "./RtmpConnectionPool";
-import fs from "node:fs";
+import Main from "./MediaServer/Main";
 
-// const tlsServer = tls.createServer(
-//   {
-//     key: fs.readFileSync(path.join(__dirname, "..", "cert", "privatekey.pem")),
-//     cert: fs.readFileSync(path.join(__dirname, "..", "cert", "cert.pem")),
-//   },
-//   (socket) => {
-//     const rtmpConnectionPool = new RtmpConnectionPool(socket);
-//     rtmpConnectionPool.start();
-//   }
-// );
-
-const server = Net.createServer((socket) => {
-  const rtmpConnectionPool = new RtmpConnectionPool(socket);
-  rtmpConnectionPool.start();
-});
-
-server.listen(1935, () => console.log("RTMP SERVER RUNNING ON PORT 1935"));
+let mediaServer = new Main();
