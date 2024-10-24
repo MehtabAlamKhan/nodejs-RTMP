978265681b9d1adbf936f4d74a2ad3640a97ee27
MehtabAlamKhan <kmehtabalam19@gmail.com>
2024-10-24 21:01:49 +0530

    hevc

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,15 @@ import Main from "./MediaServer/Main";
 let streamingServer = new Main();
 streamingServer.start();
 
-const logMemoryUsage = () => {
-  const memoryUsage: NodeJS.MemoryUsage = process.memoryUsage();
-  for (let key in memoryUsage) {
-    if (memoryUsage.hasOwnProperty(key)) {
-      console.log(`${key}: ${(memoryUsage[key as keyof NodeJS.MemoryUsage] / 1024 / 1024).toFixed(2)} MB `);
-    }
-  }
-  console.log("\r\n");
-};
+// const logMemoryUsage = () => {
+//   const memoryUsage: NodeJS.MemoryUsage = process.memoryUsage();
+//   for (let key in memoryUsage) {
+//     if (memoryUsage.hasOwnProperty(key)) {
+//       console.log(`${key}: ${(memoryUsage[key as keyof NodeJS.MemoryUsage] / 1024 / 1024).toFixed(2)} MB `);
+//     }
+//   }
+//   console.log("\r\n");
+// };
 // logMemoryUsage();
 
 // setInterval(() => {
